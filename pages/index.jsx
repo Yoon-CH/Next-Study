8471c8f6becdc76b8306b7e9c3608e506c096e34
윤창현 <changh950@gmail.com>
2022-05-05 13:27:09 +0900

    ✨ main 페이지 Fetching Data

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HeadName from "../components/HeadName";
 
+const API_KEY = "abcd538ed5842bd98a704483dbcad917";
+
 const index = () => {
+  const [movies, setMovies] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
+
   return (
     <>
       <HeadName title="Home" />
-      <h2>Home</h2>
+      {!movies && <h3>Loading...</h3>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </>
   );
 };
