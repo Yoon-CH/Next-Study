c162c60926cc01e15797c5132b6f6fdaddf1df69
윤창현 <changh950@gmail.com>
2022-05-06 10:47:20 +0900

    💄 Home 영화 라인업 마크업 구현

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,15 +18,37 @@ const index = () => {
   }, []);
 
   return (
-    <>
+    <div className="container">
       <HeadName title="Home" />
       {!movies && <h3>Loading...</h3>}
       {movies?.map((movie) => (
-        <div key={movie.id}>
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
-    </>
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
+    </div>
   );
 };
 
