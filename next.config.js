35080ea83502d67176607e56aca519f238824d78
윤창현 <changh950@gmail.com>
2022-05-07 20:54:20 +0900

    ✨ Next Redirect / Rewrite 활용

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = process.env.API_KEY;
+
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,12 @@
 import React, { useEffect, useState } from "react";
 import HeadName from "../components/HeadName";
 
-const API_KEY = "abcd538ed5842bd98a704483dbcad917";
-
 const index = () => {
   const [movies, setMovies] = useState();
 
   useEffect(() => {
     (async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json();
+      const { results } = await (await fetch(`/api/movies`)).json();
       setMovies(results);
     })();
   }, []);
